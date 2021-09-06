58671c4820ef044cdc82de2303eff461265aa859
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-09-06 16:11:44 -0300

    Feat: exercicio 3 :rocket:

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+#Escreva um programa para verificar se um elemento x está presente em uma lista.
+
+lista = ["um","dois","tres","quatro","cinco","seis","sete","oito"]
+
+print(lista.count("quatro"))
