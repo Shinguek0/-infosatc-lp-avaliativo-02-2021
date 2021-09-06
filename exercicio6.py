8847188307f5c6863f97c251433325cb8d769e7b
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-09-06 16:25:34 -0300

    Feat: exercicio 6 :rocket:

---
+++ exercicio6.py
@@ -0,0 +1,15 @@
+#8. Vá a uma loja online de computadores, faça uma lista de itens para montar um pc
+#gamer. Não se esqueça de colocar um gabinete cooler master e uma placa de
+#vídeo GeForce RTX 2080Ti.
+#A. Depois de ir para o carrinho de compras na loja online, você percebeu que
+#o gabinete cooler master e a placa GeForce RTX 2080Ti passaram do valor
+#do seu orçamento para montar seu pc gamer. Então REMOVA os dois itens
+#da sua lista.
+
+
+ListaPC = ["gabinete cooler master", "GeForce RTX 2080Ti", "INTEL CORE I9 9900", "Z390M DDR4", "16GB DDR4 GAMING"]
+
+print("Lista com todos os itens:", ListaPC)
+ListaPC.remove("gabinete cooler master")
+ListaPC.remove("GeForce RTX 2080Ti")
+print("Lista com os itens removidos:", ListaPC)
