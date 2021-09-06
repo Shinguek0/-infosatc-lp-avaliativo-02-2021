78e26d62bc096d96d9b8c1747ac3ad2b28f2989a
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2021-09-06 16:25:43 -0300

    Feat: exercicio 7 :rocket:

---
+++ exercicio7.py
@@ -0,0 +1,4 @@
+#Faça uma lista de números inteiros em ordem aleatória e mostre ao usuário a lista ordenada.
+
+Lista = [123,32,4,345,234,5,67,24]
+print("lista em ordem crescente: ", sorted(Lista))
